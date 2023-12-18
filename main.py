6ecdfe46f40c4c66adcc99e6dc4a0eb49649ed6e
tayylorngo <tyngo2013@gmail.com>
2023-12-18 13:40:42 -0500

    can now send game data to discord channel

--- main.py
+++ main.py
@@ -19,16 +19,21 @@ def run():
         logger.info(f"User: {bot.user} (ID: {bot.user.id})")
         await send_daily_message()
 
+    def get_game_data():
+        return game_requests.filter_data(game_requests.get_data())
+
     async def send_daily_message():
         now = datetime.datetime.now()
         then = now + datetime.timedelta(days=1)
         then.replace(hour=2, minute=0)
-        # then = now.replace(hour=21, minute=5)
+        # then = now.replace(hour=13, minute=37)
         wait_time = (then - now).total_seconds()
         await asyncio.sleep(wait_time)
 
+        game_data = get_game_data()
         channel = bot.get_channel(1181446708232716321)
-        await channel.send("GOOD MORNING!")
+        for game in game_data:
+            await channel.send(game)
 
     bot.run(settings.TOKEN, root_logger=True)
 
